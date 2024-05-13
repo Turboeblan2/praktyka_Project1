1f2ff98834faf44c2e921b98a6c340d59382ec03
Zosia Samosia <Zosia@Samosia.pl>
2024-05-13 13:33:26 +0200

    add linksMenu hide

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,21 @@
 
 const burger = document.querySelector('nav .material-icons');
 
-const close = document.querySelector('.navigation .material-icons')
+const close = document.querySelector('.navigation .material-icons');
 
 const menuDiv = document.querySelector('.navigation');
 
 
 burger.addEventListener('click', () => {
      menuDiv.classList.add('showMenu');
-
-
-
-     
 })
 close.addEventListener('click', ()=>{
     menuDiv.classList.remove('showMenu');
-} )
+})
+
+const linksMenu = document.querySelectorAll('.navigation a');
+linksMenu.forEach(elem =>{
+    elem.addEventListener('click', ()=>{
+        menuDiv.classList.remove('showMenu');
+    })
+})
