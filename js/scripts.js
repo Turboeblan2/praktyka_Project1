90e80cf2dd79c1be5fe4acc2244e8bc4e64a405d
Zosia Samosia <Zosia@Samosia.pl>
2024-05-13 12:39:18 +0200

    add config

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const burger = document.querySelector('nav .material-icons');
+
+const close = document.querySelector('.navigation .material-icons')
+
+const menuDiv = document.querySelector('.navigation');
+
+
+burger.addEventListener('click', () => {
+     menuDiv.classList.add('showMenu');
+
+
+
+     
+})
+close.addEventListener('click', ()=>{
+    menuDiv.classList.remove('showMenu');
+} )
